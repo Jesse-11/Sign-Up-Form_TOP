97027a4173694233ccd2e8247087d9e1df23f5ca
Jesse-11 <jesse.hopppo@gmail.com>
2024-04-14 22:27:46 +0930

    Changd the Basic images layout of my page.
    Added new dark mode buttons and icons. Changed picture to fill the background rather then be the icon.
    Also aded some change in color to buttons and links when in dark mode.

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+
+
+/*
 const toggleButton = document.getElementById('toggle-mode'); 
-const body = document.body;
 
 toggleButton.addEventListener('click', function() {
+    body.classList.toggle('dark-mode');
+});
+*/
+
+const themeButton = document.querySelector('.theme-button');
+const sunIcon = document.querySelector('.sun-icon');
+const moonIcon = document.querySelector('.moon-icon');
+const body = document.body;
+
+
+
+
+
+themeButton.addEventListener('click', () => {
+    if (moonIcon.classList.contains('active')) {
+        moonIcon.classList.remove('active');
+        sunIcon.classList.add('active');
+    } else {
+        sunIcon.classList.remove('active');
+        moonIcon.classList.add('active');
+    }
     body.classList.toggle('dark-mode');
 });
